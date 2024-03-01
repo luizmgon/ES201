a7dea9aa418cd4ca77d65a05b29516aa654ac93a
AugustoMdeP <augusto.miranda@ensta-paris.fr>
2024-03-01 18:49:25 +0100

    fasfa

--- TP4/dijkstra/script_outorder.sh
+++ TP4/dijkstra/script_outorder.sh
@@ -9,45 +9,27 @@ PRG_SS="dijkstra_small.ss"
 
 mkdir -p simu_out
 
-
-#for TYPE in nottaken taken perfect bimod 2lev
-#do	
-#	OPTIONS="-bpred $TYPE"
-#	REDIR_OUT_SIMU="-redir:sim simu/simu_${TYPE}.out"
-#	$SIM_OUT_ORDER $OPTIONS $REDIR_OUT_SIMU $PRG_SS
-#done
-
-#for SIZE in 16 32 64 128
-#do	
-#	OPTIONS="-ruu:size ${SIZE}"
-#	REDIR_OUT_SIMU="-redir:sim simu/simu_ruu${SIZE}.out"
-#	$SIM_OUT_ORDER $OPTIONS $REDIR_OUT_SIMU $PRG_SS
-#done
-
 A7_IL2="-cache:il2 dl2"
 A7_DL2="-cache:dl2 ul2:2048:32:8:l"
 A15_IL2="-cache:il2 dl2"
 A15_DL2="-cache:dl2 ul2:512:64:16:l"
 
+#for SIZE in 16 32 64 128 256
+#do
+#	OPTIONS="-bpred bimod -fetch:mplat 8 -fetch:ifqsize 4 -decode:width 2 -issue:width 4 -commit:width 2 -ruu:size 2 -lsq:size 8 -res:ialu 1 -res:fpalu 1 -res:fpmult 1 -res:imult 1"
+#	A7_IL1="-cache:il1 il1:$SIZE:32:2:l"
+#	A7_DL1="-cache:dl1 dl1:$SIZE:32:2:l"
+#	REDIR_OUT_SIMU1="-redir:sim simu_out/simu_cache_A7_${SIZE}.out"
+#	$SIM_OUT_ORDER $OPTIONS $A7_IL1 $A7_DL1 $A7_IL2 $A7_DL2 $REDIR_OUT_SIMU1 $PRG_SS
+#done
+
 for SIZE in 16 32 64 128 256
-do
-	OPTIONS="-bpred bimod -fetch:mplat 8 -fetch:ifqsize 4 -decode:width 2 -issue:width 4 -commit:width 2 -ruu:size 2 -lsq:size 8 -res:ialu 1 -res:fpalu 1 -res:fpmult 1 -res:imult 1"
-	A7_IL1="-cache:il1 il1:$SIZE:32:2:l"
-	A7_DL1="-cache:dl1 dl1:$SIZE:32:2:l"
-#	SIZE_A15=$(SIZE*2)
-#	A15_IL1="-cache:il1 il1:$SIZE_A15::64:2"
-	REDIR_OUT_SIMU1="-redir:sim simu_out/simu_cache_A7_${SIZE}.out"
-#	REDIR_OUT_SIMU2="-redir:sim simu_out/simu_cache_A15_${SIZE_A15}.out"
-	$SIM_OUT_ORDER $OPTIONS $A7_IL1 $A7_DL1 $A7_IL2 $A7_DL2 $REDIR_OUT_SIMU1 $PRG_SS
-#	$SIM_CACHE $OPTIONS $A15_IL1 $A15_DL1 $A15_IL2 $A15_DL2 $REDIR_OUT_SIMU2 $PRG_SS
-
-done
-
-for SIZE in 32 64 128 256 512
 do
 	OPTIONS="-bpred 2lev -fetch:mplat 15 -fetch:ifqsize 8 -decode:width 4 -issue:width 8 -commit:width 4 -ruu:size 16 -lsq:size 16 -res:ialu 5 -res:fpalu 1 -res:imult 1 -res:fpmult 1"
 	A15_IL1="-cache:il1 il1:$SIZE:64:2:l"
-	A15
-
+	A15_DL1="-cache:dl1 dl1:$SIZE:64:2:l"
+	REDIR_OUT_SIMU="-redir:sim simu_out/simu_cache_A15_${SIZE}.out"
+	$SIM_OUT_ORDER $OPTIONS $A15_IL1 $A15_DL1 $A15_IL2 $A15_DL2 $REDIR_OUT_SIMU $PRG_SS
+done
 	
 
